e3ce09ed46149953d025a60d5673368dc3cb5747
Maks Kaplunovsky <kaplunovskymv@gmail.com>
2017-12-01 15:20:54 +0200

    FilterModule acl_system configured

---
+++ app/code/Filters/Customization/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Amasty Team
+ * @copyright Copyright (c) 2017 Amasty (https://www.amasty.com)
+ * @package Amasty_Xlanding
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Filters_Customization',
+    __DIR__
+);
